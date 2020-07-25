0236be3717b7bda00fed01ca723a693a7e7b5672
Alex Fabiano <afabiano15@gmail.com>
2020-07-25 14:39:53 -0400

    criação do projeto

---
+++ src/test/java/com/meloafc/people/PeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meloafc.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
